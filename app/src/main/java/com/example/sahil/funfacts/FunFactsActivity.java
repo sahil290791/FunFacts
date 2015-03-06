e3cfddd35784f534ad508725af4435637cc5c1fc
SAHIL_PRAJAPATI <sahilprjpt206@gmail.com>
2015-03-06 08:55:39 +0530

    Added App icon

--- app/src/main/java/com/example/sahil/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/example/sahil/funfacts/FunFactsActivity.java
@@ -3,18 +3,20 @@ package com.example.sahil.funfacts;
 import android.graphics.drawable.Drawable;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Random;
 
 
 public class FunFactsActivity extends ActionBarActivity {
-
+    public static final String TAG = FunFactsActivity.class.getSimpleName();
     private FactBook mFactBook = new FactBook();
     private Colors mColor = new Colors();
     @Override
@@ -38,6 +40,8 @@ public class FunFactsActivity extends ActionBarActivity {
         };
 
         showFactButton.setOnClickListener(listner);
+        //Toast.makeText(this, "Yay! Our Activity was created!", Toast.LENGTH_LONG).show();
+        Log.d(TAG,"Logging out onCreate() method");
 
     }
 
